25cee4fc57ae2a73d925c62f728b9aab68dbb60b
David Tan <David@Davids-Mac.local>
2018-04-13 01:57:40 -0700

    Added scrollview

---
+++ What2Cook/ViewControllers/SingleViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SingleViewController.swift
+//  What2Cook
+//
+//  Created by David Tan on 4/13/18.
+//  Copyright © 2018 hyelim. All rights reserved.
+//
+
+import UIKit
+
+class SingleViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
