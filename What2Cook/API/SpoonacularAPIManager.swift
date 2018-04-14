7d77429ab41f19d7832ee82c12fe0db8c6d3c9f5
ejoun6718 <hjoun@ucsd.edu>
2018-04-13 18:52:04 -0700

    Added SpoonacularAPIManager file

---
+++ What2Cook/API/SpoonacularAPIManager.swift
@@ -0,0 +1,16 @@
+//
+//  SpoonacularAPIManager.swift
+//  What2Cook
+//
+//  Created by Hye Lim Joun on 4/13/18.
+//  Copyright © 2018 hyelim. All rights reserved.
+//
+
+import Foundation
+
+class SpoonacularAPIManager {
+  
+  func searchRecipes(_ ingredients: [String], completion: @escaping([Recipe]?, Error?) -> ()) {
+    
+  }
+}
---
+++ What2Cook/Models/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredient.swift
+//  What2Cook
+//
+//  Created by Hye Lim Joun on 4/13/18.
+//  Copyright © 2018 hyelim. All rights reserved.
+//
+
+import Foundation
+
+class Ingredient {
+  var name: String!
+  var category: String!
+}
