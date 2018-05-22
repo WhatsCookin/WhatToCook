a69d8e5f39290df83cfc41d1a16cc20cd2543b28
ejoun6718 <hjoun@ucsd.edu>
2018-05-21 21:48:28 -0700

    Remove unneeded files

--- What2Cook/API/SpoonacularAPIManager.swift
+++ What2Cook/API/SpoonacularAPIManager.swift
@@ -94,7 +94,7 @@ class SpoonacularAPIManager {
     func getPopularRecipes(completion: @escaping([RecipeItem]?, Error?) -> ()) {
     //let key = "69p5QHDqZfmshevTW4RVD0dwIh7Qp1L5vUZjsnVjlWJFfVpmAb"
     
-    let numRecipes = 10 // number of popular recipes to be returned
+    /*let numRecipes = 10 // number of popular recipes to be returned
     
     let urlstring = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?number=" + String(numRecipes)
     
@@ -114,7 +114,7 @@ class SpoonacularAPIManager {
         else {
             print("Something went wrong")
         }
-    }
+    }*/
     
   }
     
