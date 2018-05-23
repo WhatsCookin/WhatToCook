33c83ca9307793e633ba5622248b5a6ad17210e5
ejoun6718 <hjoun@ucsd.edu>
2018-05-23 01:26:33 -0700

    Fixed autolayout for instructions, added new icons, changed images to aspect fill

--- What2Cook/ViewControllers/SingleViewController.swift
+++ What2Cook/ViewControllers/SingleViewController.swift
@@ -13,7 +13,14 @@ import Speech
 class SingleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, AVSpeechSynthesizerDelegate, SFSpeechRecognizerDelegate {
   
   @IBOutlet weak var tableViewIngredients: UITableView! // tableView for ingredients in the recipe
-  @IBOutlet weak var tableViewDirections: UITableView! // tableview for directions in the recipe
+  // tableview for directions in the recipe
+  @IBOutlet weak var tableViewDirections: UITableView! {
+    didSet {
+      tableViewDirections.rowHeight = UITableViewAutomaticDimension
+      tableViewDirections.estimatedRowHeight = 50
+    }
+  }
+  
   @IBOutlet weak var recipeImage: UIImageView!
   @IBOutlet weak var recipeName: UILabel!
   @IBOutlet weak var recipeTime: UILabel!
