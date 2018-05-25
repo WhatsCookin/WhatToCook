9079e6e23c83e239a7dfe99d1d042590b5f7992e
ejoun6718 <hjoun@ucsd.edu>
2018-05-25 01:11:37 -0700

    Add voice commands for fridge view

--- What2Cook/ViewControllers/FridgeViewController.swift
+++ What2Cook/ViewControllers/FridgeViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 import Parse
+import Speech
 
-class FridgeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, ExpandableHeaderViewDelegate {
+class FridgeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, ExpandableHeaderViewDelegate, SFSpeechRecognizerDelegate {
   
   var recipesList: [Recipe] = []
   var ingredients: [String] = []
   var selectIndexPath: IndexPath!
+  private var ignoredChars = 0  // For continuous speech recognition
+  private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+  private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+  private var recognitionTask: SFSpeechRecognitionTask?
+  private let audioEngine = AVAudioEngine()
   
+  @IBOutlet weak var microphoneButton: UIButton!
   @IBOutlet weak var tableView: UITableView!
+  
+  @IBAction func microphoneTapped(_ sender: UIButton) {
+    sender.isSelected = !sender.isSelected
+    
+    if audioEngine.isRunning {
+      audioEngine.stop()
+      recognitionRequest?.endAudio()
+      microphoneButton.isEnabled = false
+    } else {
+      startRecording()
+    }
+  }
+  
   @IBAction func onSearch(_ sender: Any) {
     if checkForSelection() {
       SpoonacularAPIManager().searchRecipes(ingredients) { (recipes, error) in
@@ -242,6 +262,141 @@ class FridgeViewController: UIViewController, UITableViewDelegate, UITableViewDa
       }
       print(ingredients)
     }*/
+      
+      microphoneButton.isEnabled = false
+      
+      speechRecognizer?.delegate = self
+      
+      SFSpeechRecognizer.requestAuthorization { (authStatus) in
+        
+        var isButtonEnabled = false
+        
+        switch authStatus {
+        case .authorized:
+          isButtonEnabled = true
+          
+        case .denied:
+          isButtonEnabled = false
+          print("User denied access to speech recognition")
+          
+        case .restricted:
+          isButtonEnabled = false
+          print("Speech recognition restricted on this device")
+          
+        case .notDetermined:
+          isButtonEnabled = false
+          print("Speech recognition not yet authorized")
+        }
+        
+        OperationQueue.main.addOperation() {
+          self.microphoneButton.isEnabled = isButtonEnabled
+        }
+      }
+  }
+  
+  func startRecording() {
+    if recognitionTask != nil {
+      recognitionTask?.cancel()
+      recognitionTask = nil
+    }
+    
+    let audioSession = AVAudioSession.sharedInstance()
+    do {
+      try audioSession.setCategory(AVAudioSessionCategoryRecord)
+      try audioSession.setMode(AVAudioSessionModeMeasurement)
+      try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+    } catch {
+      print("audioSession properties weren't set because of an error.")
+    }
+    
+    recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+    
+    let inputNode = audioEngine.inputNode
+    
+    guard let recognitionRequest = recognitionRequest else {
+      fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+    }
+    
+    recognitionRequest.shouldReportPartialResults = true
+    
+    recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+      
+      var isFinal = false
+      
+      // Start parsing voice command
+      if result != nil {
+        var voiceCommand = result!.bestTranscription.formattedString.substring(from: self.ignoredChars) ?? ""
+        print("string: " + result!.bestTranscription.formattedString)
+        print(self.ignoredChars)
+        
+        voiceCommand = voiceCommand.replacingOccurrences(of: "Add ", with: " add ")
+        voiceCommand = voiceCommand.replacingOccurrences(of: "At ", with: " add ")
+        voiceCommand = voiceCommand.replacingOccurrences(of: " at ", with: " add ")
+        voiceCommand = voiceCommand.replacingOccurrences(of: "To ", with: " to ")
+        voiceCommand = voiceCommand.replacingOccurrences(of: "Two ", with: " to ")
+        voiceCommand = voiceCommand.replacingOccurrences(of: " two ", with: " to ")
+        
+        print(voiceCommand)
+        
+        if (voiceCommand.range(of: " add ", options:NSString.CompareOptions.backwards) != nil) && voiceCommand.range(of: " to ") != nil {
+          // Parse Ingredient
+          let addRange = voiceCommand.rangeEndIndex(toFind: " add ")
+          voiceCommand = voiceCommand.substring(from: addRange)!
+          
+          print(voiceCommand)
+          let ingredient = voiceCommand
+          print("ingredient: " + ingredient)
+          self.ignoredChars = result!.bestTranscription.formattedString.count
+          self.addIngredient(ingredient: ingredient)
+          
+          // Parse Category
+          let toEndRange = voiceCommand.rangeEndIndex(toFind: " to ")
+          let category = voiceCommand.substring(from: toEndRange)!
+          
+          let categoryIndex = self.checkCategoryExists(category: category)
+          if categoryIndex != -1 {
+            // Ignore capitalization
+            self.addIngredient(ingredient: ingredient, category: self.sections[categoryIndex].category)
+          }
+          self.ignoredChars = result!.bestTranscription.formattedString.count
+        }
+        
+        print(voiceCommand)
+        
+        isFinal = (result?.isFinal)!
+      }
+      
+      if error != nil || isFinal {
+        self.audioEngine.stop()
+        inputNode.removeTap(onBus: 0)
+        
+        self.recognitionRequest = nil
+        self.recognitionTask = nil
+        
+        self.microphoneButton.isEnabled = true
+      }
+    })
+    
+    let recordingFormat = inputNode.outputFormat(forBus: 0)
+    inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+      self.recognitionRequest?.append(buffer)
+    }
+    
+    audioEngine.prepare()
+    
+    do {
+      try audioEngine.start()
+    } catch {
+      print("audioEngine couldn't start because of an error.")
+    }
+  }
+  
+  func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+    if available {
+      microphoneButton.isEnabled = true
+    } else {
+      microphoneButton.isEnabled = false
+    }
   }
   
   override func didReceiveMemoryWarning() {
