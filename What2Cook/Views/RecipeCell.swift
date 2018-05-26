895437ec9e23a3f7cad370e0c884642482a5321c
ejoun6718 <hjoun@ucsd.edu>
2018-05-26 04:49:58 -0700

    Change fridge view UI

--- What2Cook/Views/RecipeCell.swift
+++ What2Cook/Views/RecipeCell.swift
@@ -29,7 +29,7 @@ class RecipeCell: UICollectionViewCell {
             servingsLabel.text = String(recipe.servings)
         }
     }
-    
+  
     override func layoutSubviews() {
         //reload images
       super.layoutSubviews()
