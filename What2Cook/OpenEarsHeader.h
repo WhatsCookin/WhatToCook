b547752efc456bda3d6b84ac2f8bf7bc11815291
ejoun6718 <hjoun@ucsd.edu>
2018-06-04 19:19:54 -0700

    Add OpenEars framework to project

---
+++ What2Cook/OpenEarsHeader.h
@@ -0,0 +1,20 @@
+//
+//  OpenEarsHeader.h
+//  What2Cook
+//
+//  Created by Hye Lim Joun on 6/4/18.
+//  Copyright © 2018 hyelim. All rights reserved.
+//
+
+#ifndef OpenEarsHeader_h
+#define OpenEarsHeader_h
+
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h> // Only needed if using Flite speech
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+
+#endif /* OpenEarsHeader_h */
