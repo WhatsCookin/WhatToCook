414df569cebead801c2f40852676d7eae1b6ac9e
ejoun6718 <hjoun@ucsd.edu>
2018-07-08 17:15:36 -0700

    Change fridge view appearance

--- What2Cook/Views/IngredientCell.swift
+++ What2Cook/Views/IngredientCell.swift
@@ -19,7 +19,7 @@ class IngredientCell: UITableViewCell {
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
-    //self.accessoryType = selected ? .checkmark : .none
-    checkmark.isHidden = !checkmark.isHidden
+    self.accessoryType = selected ? .checkmark : .none
+    //checkmark.isHidden = !checkmark.isHidden
   }
 }
